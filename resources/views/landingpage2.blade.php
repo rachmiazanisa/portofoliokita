d18c0b9c8715455b6169f4b4a3e205c46d14631b
rachmiazanisa <rachmi.azanisa@gmail.com>
2018-03-23 15:04:52 +0700

    progress portofolio & about us

--- resources/views/landingpage2.blade.php
+++ resources/views/landingpage2.blade.php
@@ -8,13 +8,62 @@
         <h1>Our Portfolio</h1>
         <h3>Dhia Aziz  &amp; Rachmi Azanisa</h3>
         <br>
-        <a href="#about" class="btn btn-dark btn-lg js-scroll-trigger">Find Out More</a>
       </div>
     </header>
+
+
+  <section class = " bg-portfolio">
+      <div class = "container">
+        <div class = "row align-items-center">
+        <div class="col-lg-10 text-vertical-center text-judul mx-auto">
+          <h2>Portfolio</h2>
+          </div>
+          <div class = "col-lg-10 mx-auto">
+              <div id="carouselExampleControls" class="carousel slide img-shadow" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100" src="{{asset('/img/portfolio-1.jpg')}}" alt="First slide">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Judul</h5>
+                <p>Deskripsi</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="{{asset('/img/portfolio-2.jpg')}}" alt="Second slide">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Judul</h5>
+                <p>Deskripsi</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="{{asset('/img/portfolio-3.jpg')}}" alt="Third slide">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Judul</h5>
+                <p>Deskripsi</p>
+              </div>
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+      </div>
+          </div>
+        </div>
+      </div>
+  </section>
+
 
     <section id = "aboutus">
       <div class="container">
         <div class="row align-items-center">
+        <div class="col-lg-10 text-vertical-center text-judul mx-auto">
+          <h2>Team</h2>
+          </div>
           <div class="col-md-6 order-2">
             <div class="p-5">
               <img class="img-fluid rounded-circle" src="/img/aziz.jpg" alt="">
